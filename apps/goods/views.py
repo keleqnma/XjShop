34a02d772c9f58a59486f8c0b89d92ac9e4aeefa
gwgwgw <your email>
2019-04-01 20:47:59 +0800

    商品列表页api 100%

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -7,7 +7,7 @@ from rest_framework import viewsets
 from rest_framework.viewsets import GenericViewSet
 
 class GoodsPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 15
     page_size_query_param = 'page_size'
     page_query_param = "p"
     max_page_size = 100
