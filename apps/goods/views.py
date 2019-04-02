c566a3bcafbb25d88029c1083887b55fe34b28f2
gwgwgw <your email>
2019-04-02 14:42:27 +0800

    0402 增添过滤类

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,21 +1,34 @@
 from .serializers import GoodsSerializer
 from rest_framework import mixins
-from rest_framework import generics
-from .models import Goods
+from rest_framework import filters
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
-from rest_framework.viewsets import GenericViewSet
+from .models import Goods
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import GoodsFilter
 
 class GoodsPagination(PageNumberPagination):
-    page_size = 15
+    page_size = 12
     page_size_query_param = 'page_size'
     page_query_param = "p"
     max_page_size = 100
 
 class GoodsViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
-   商品列表页,可以查看商品
+   商品列表页,分页， 搜索， 过滤， 排序
     """
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
     pagination_class = GoodsPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    filter_class=GoodsFilter
+    search_fields = ('name', 'goods_brief', 'goods_desc')
+    ordering_fields = ('sold_num', 'shop_price')
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.click_num += 1
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
